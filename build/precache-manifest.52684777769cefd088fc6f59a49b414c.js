8845b68fd9ba964031a7abc5848aa930fe14582c
Derek Glassick <derek.glassick@gmail.com>
2020-07-01 21:50:38 -0700

    pushing build folder

---
+++ build/precache-manifest.52684777769cefd088fc6f59a49b414c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e4da28332437c68980f12349431e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7465484162f5d96b0664",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e964064369fcdbb63bff",
+    "url": "/static/js/2.889b63d4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.889b63d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7465484162f5d96b0664",
+    "url": "/static/js/main.6fa7b04f.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
